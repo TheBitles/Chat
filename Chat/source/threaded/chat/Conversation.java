bc8f4fa804ad8ba7a123005e5d0c8ca4133cf5ff
Dimas <seymoure@gmail.com>
2017-06-04 16:24:29 -0300

    start

---
+++ Chat/source/threaded/chat/Conversation.java
@@ -0,0 +1,46 @@
+package threaded.chat;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Conversation extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 551908498875986417L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Conversation frame = new Conversation();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Conversation() {
+		setTitle("second Window dude");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
---
+++ Chat/source/threaded/chat/Main.java
@@ -0,0 +1,13 @@
+package threaded.chat;
+public class Main {
+	
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+		ventanaPrincipal.setVisible(true);
+	}
+
+}
