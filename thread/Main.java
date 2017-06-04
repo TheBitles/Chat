1f3b79b1ea56424ccfdd42b66fbabc692481729a
Dimas <seymoure@gmail.com>
2017-06-04 15:18:53 -0300

    Un buen comienzo

---
+++ thread/Main.java
@@ -0,0 +1,13 @@
+package mi.thread;
+public class Main {
+	
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+		ventanaPrincipal.setVisible(true);
+	}
+
+}
